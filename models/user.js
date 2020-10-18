c63ba91bc5063482c9a19ecaf8afd57a2169d497
ezequiel <ezeedge@gmail.com>
2020-10-18 03:44:17 -0300

    final

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 
 const mongoose = require("mongoose");
-const uuidv1 = require("uuid/v1");
+const uuidv1 = require("uuidv1");
+//const {v1: uuidv1} = require('uuid/v1')
 const crypto = require("crypto");
 const { ObjectId } = mongoose.Schema;
 const Post = require("./post");
